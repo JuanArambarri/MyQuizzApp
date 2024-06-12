1b713d6a882a9e116768ae4c1c5a0658301809ad
Juan Bautista Arambarri <jbat1997@gmail.com>
2024-06-12 18:50:23 -0300

    Implement solution to repository
    Adding MyQuizzApp Solution to new repository

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyQuizzApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Question[] questions = new Question[]
+        {
+            new Question(
+                "What is the capital of Germany?",
+                new String[] { "Paris", "Berlin", "London", "Madrid" },
+                1)
+        };
+        Quizz myQuizz = new Quizz(questions);
+        myQuizz.DisplayQuestion(questions[0]);
+        /* Preliminary try to check for answer.
+        int choice = int.Parse(Console.ReadLine());
+        if(questions[0].isCorrectAnswer(choice)){
+        cw "You have guessed right";
+        }else{"You have guess wrong";}
+        */
+    }
+}
---
+++ Quizz.cs
@@ -0,0 +1,29 @@
+﻿namespace MyQuizzApp;
+
+public class Quizz
+{
+    private Question[] questions;
+
+    public Quizz(Question[] questions) //the array will be run in another class
+    {
+        this.questions = questions;
+    }
+
+    public void DisplayQuestion(Question question) //should be private. (Made public for testing)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+        Console.WriteLine("║                                 Question                                ║");
+        Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+        Console.ResetColor();
+        Console.WriteLine(question.QuestionText); //taking advantage of the property
+        for (int i = 0; i < question.Answers.Length; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("   "); //create fake indentation
+            Console.Write(i+1); //Question number with different colour
+            Console.ResetColor(); //makes text normal again
+            Console.WriteLine($". {question.Answers[i]}");
+        }
+    }
+}
